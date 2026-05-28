b09cd8b0657c61824b4ff47dd55be2c1595d4d10
brunoarcecabral <bruno.arcecabralx@gmail.com>
2026-05-28 20:03:02 -0300

    asd

---
+++ Source/Jalea/Jalea.Build.cs
@@ -0,0 +1,44 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Jalea : ModuleRules
+{
+	public Jalea(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"Jalea",
+			"Jalea/Variant_Horror",
+			"Jalea/Variant_Horror/UI",
+			"Jalea/Variant_Shooter",
+			"Jalea/Variant_Shooter/AI",
+			"Jalea/Variant_Shooter/UI",
+			"Jalea/Variant_Shooter/Weapons"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
